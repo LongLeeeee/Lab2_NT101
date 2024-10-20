2eb43bcd9d649b359619ec729963a0a70437bc08
longle <22520813@gm.uit.edu.vn>
2024-10-20 21:17:49 +0700

    1.3 completed

--- Task_C_1.3.py
+++ Task_C_1.3.py
@@ -1,2 +1,18 @@
-number =  pow(10,44,19)
-print(number)
+
+def pow1(a, x, p):
+    result = 1
+    a = a % p
+
+    while x > 0:
+        if x % 2 == 1: 
+            result = (result*a) % p
+        x//=2
+        a = (a*a) % p
+    return result
+
+number1 = int(input("Enter the base (a): "))
+number2 = int(input("Enter the exponent (x): "))
+number3 = int(input("Enter the modulus (p): "))
+
+number =  pow1(number1,number2,number3)
+print(f"Result: {number}")
