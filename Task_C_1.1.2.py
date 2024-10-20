dfe84e13f00b22d52ae4f375e3a279d9a3be42e7
longle <22520813@gm.uit.edu.vn>
2024-10-20 21:17:16 +0700

    1.1.2 completed

--- Task_C_1.1.2.py
+++ Task_C_1.1.2.py
@@ -1,8 +1,9 @@
 import math
 import random
 
-n = 100
+n = 5000
 prime_matrix = [True]*n 
+
 def eratosthenes(primes):
     for i in range(2, n):
         if prime_matrix[i] == True:
@@ -17,8 +18,6 @@ def isprime(prime):
         return True
     if prime % 2 == 0:
         return False
-    if prime % 3 == 0:
-        return False
     r= 0
     d = prime - 1
     while d % 2 == 0:
@@ -28,7 +27,7 @@ def isprime(prime):
     for _ in range(5):
         a = random.randint(2, prime - 2) 
         x = pow(a, d, prime) 
-        if x == 1 or x == prime - 1:
+        if x == 1 or x == prime -1:
             continue
         for _ in range(r - 1):
             x = pow(x, 2, prime) 
@@ -38,18 +37,18 @@ def isprime(prime):
             return False
     return True 
 
-def find_mersenne_primes(matrix, count):
+def find_mersenne_primes(matrix):
+    count = 1
     for i in matrix:
        mersenne_prime = 2**i - 1
        if isprime(mersenne_prime) == True:
-           count+=1 
-           print(f"{mersenne_prime}")
-           if count == 10:
+            print(f"Number {count}: {mersenne_prime}")
+            count+=1 
+            if count == 21:
                break
 
-count = 10
 primes = []
 eratosthenes(primes)
-print("10 số nguyên tố Mersenne đầu tiên:")
-find_mersenne_primes(primes, count)
+print("20 số nguyên tố Mersenne đầu tiên:")
+find_mersenne_primes(primes)
 
